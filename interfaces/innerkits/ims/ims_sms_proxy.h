3d0269d295c23f85e4888613dbd1ea1148ea94f1
lihongjin <lihongjin1@huawei.com>
2022-04-18 14:56:21 +0800

    feature: ims_service短信框架
    Signed-off-by: lihongjin <lihongjin1@huawei.com>
    Change-Id: Ibb5b19790c53ee2f3c34cfdf7741cc3ccd765171

---
+++ interfaces/innerkits/ims/ims_sms_proxy.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TELEPHONY_IMS_SMS_PROXY_H
+#define TELEPHONY_IMS_SMS_PROXY_H
+
+#include "ims_sms_interface.h"
+#include "iremote_proxy.h"
+
+#include "telephony_log_wrapper.h"
+
+namespace OHOS {
+namespace Telephony {
+class ImsSmsProxy : public IRemoteProxy<ImsSmsInterface> {
+public:
+    explicit ImsSmsProxy(const sptr<IRemoteObject> &impl) : IRemoteProxy<ImsSmsInterface>(impl) {}
+    ~ImsSmsProxy() = default;
+    int32_t ImsSendMessage() override;
+    int32_t RegisterImsSmsCallback(const sptr<ImsSmsCallbackInterface> &callback) override;
+
+private:
+    static inline BrokerDelegator<ImsSmsProxy> delegator_;
+};
+} // namespace Telephony
+} // namespace OHOS
+
+#endif // TELEPHONY_IMS_SMS_PROXY_H
