386f2848f92c711ae934211f9c63661386a82f57
dingxiaochen <dingxiaochen@huawei.com>
2022-12-05 16:07:49 +0800

    fix fuzz test.
    Signed-off-by: dingxiaochen <dingxiaochen@huawei.com>

--- test/fuzztest/addsimmessage_fuzzer/addsimmessage_fuzzer.cpp
+++ test/fuzztest/addsimmessage_fuzzer/addsimmessage_fuzzer.cpp
@@ -132,7 +132,7 @@ void HasSmsCapability(const uint8_t *data, size_t size)
 
 void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
 {
-    if (data == nullptr || size <= 0) {
+    if (data == nullptr || size == 0) {
         return;
     }
 
