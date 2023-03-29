72ee1008f2e70b8402a3e610c4dee7518574c64f
wangwei30043812 <wangwei1033@huawei.com>
2023-03-29 18:35:06 +0800

    fix sms spectial character
    Signed-off-by: wangwei30043812 <wangwei1033@huawei.com>

--- utils/msg_text_convert.cpp
+++ utils/msg_text_convert.cpp
@@ -984,9 +984,6 @@ int MsgTextConvert::EscapeGSM7BitToUCS2(
     }
     if (GSM7_BIT_TO_UC_S2[pSrcText[index]] == 0x001B) {
         index++;
-        if (index >= srcLen-1) {
-            return srcLen-1;
-        }
         result = EscapeToUCS2(pSrcText[index], pLangInfo);
     } else {
         result = GSM7_BIT_TO_UC_S2[pSrcText[index]];
