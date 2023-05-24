858c1d03f7ba7dec10c53a9cc266a2e10912c8bd
liuxiyao223 <liuxiyao223@huawei.com>
2023-05-24 20:11:09 +0800

    codex Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/sms_sender.cpp
+++ services/sms_sender.cpp
@@ -343,7 +343,7 @@ void SmsSender::SetNetworkState(bool isImsNetDomain, int32_t voiceServiceState)
 {
     isImsNetDomain_ = isImsNetDomain;
     voiceServiceState_ = voiceServiceState;
-    if (enableImsSmsOnceWhenImsReg_ && isImsNetDomain_) {
+    if (enableImsSmsOnceWhenImsReg_ && isImsNetDomain) {
         SetImsSmsConfig(slotId_, IMS_SMS_ENABLE);
         enableImsSmsOnceWhenImsReg_ = false;
     }
