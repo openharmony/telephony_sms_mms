adb15cf63dc8c68309c39f27a16403f140e4a11f
dingxiaochen <dingxiaochen@huawei.com>
2023-07-06 17:25:50 +0800

    fix interface code.
    Signed-off-by: dingxiaochen <dingxiaochen@huawei.com>

---
+++ interfaces/innerkits/delivery_short_message_callback_ipc_interface_code.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DELIVERY_SHORT_MESSAGE_CALLBACK_IPC_INTERFACE_CODE_H
+#define DELIVERY_SHORT_MESSAGE_CALLBACK_IPC_INTERFACE_CODE_H
+
+/* SAID:4008 */
+namespace OHOS {
+namespace Telephony {
+enum class DeliveryShortMessageCallbackInterfaceCode {
+    ON_SMS_DELIVERY_RESULT,
+};
+} // namespace Telephony
+} // namespace OHOS
+#endif // DELIVERY_SHORT_MESSAGE_CALLBACK_IPC_INTERFACE_CODE_H
