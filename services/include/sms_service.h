9a323bc6c77603f3e641a011c4d3b08c7bd4feb8
wangwei30043812 <wangwei1033@huawei.com>
2023-07-13 19:14:48 +0800

    sendmessage
    Signed-off-by: wangwei30043812 <wangwei1033@huawei.com>

--- services/include/sms_service.h
+++ services/include/sms_service.h
@@ -35,6 +35,7 @@ public:
     void OnStop() override;
     int32_t Dump(std::int32_t fd, const std::vector<std::u16string> &args) override;
     std::string GetBindTime();
+    void InsertSessionAndDetail(int32_t slotId, const std::string &telephone, const std::string &text);
 
     /**
      * Sends a text Type SMS message.
@@ -302,7 +303,6 @@ private:
     bool ValidDestinationAddress(std::string desAddr);
     void TrimSmscAddr(std::string &sca);
     bool CheckSimMessageIndexValid(int32_t slotId, uint32_t msgIndex);
-    void InsertSessionAndDetail(int32_t slotId, const std::string &telephone, const std::string &text);
     void InsertSmsMmsInfo(int32_t slotId, uint16_t sessionId, const std::string &number, const std::string &text);
     bool InsertSession(bool isNewSession, uint16_t messageCount, const std::string &number, const std::string &text);
     bool QuerySessionByTelephone(const std::string &telephone, uint16_t &sessionId, uint16_t &messageCount);
@@ -316,7 +316,6 @@ private:
     std::shared_ptr<SmsStateHandler> smsStateHandler_;
     const std::string SMS_MMS_INFO = "datashare:///com.ohos.smsmmsability/sms_mms/sms_mms_info";
     const std::string SMS_SESSION = "datashare:///com.ohos.smsmmsability/sms_mms/session";
-    const std::string MMS_APP = "com.ohos.mms";
 };
 } // namespace Telephony
 } // namespace OHOS
