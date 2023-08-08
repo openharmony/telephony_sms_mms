2b263f51099b744a8c6ab5900f5d1853d192850d
liuxiyao223 <liuxiyao223@huawei.com>
2023-08-08 15:20:08 +0800

    彩信收发管理函数实现 Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

---
+++ services/mms/mms_receive_manager.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mms_receive_manager.h"
+
+#include "string_utils.h"
+#include "telephony_log_wrapper.h"
+
+namespace OHOS {
+namespace Telephony {
+using namespace std;
+MmsReceiveManager::MmsReceiveManager(const std::shared_ptr<AppExecFwk::EventRunner> &runner, int32_t slotId)
+    : AppExecFwk::EventHandler(runner), slotId_(slotId)
+{}
+
+MmsReceiveManager::~MmsReceiveManager() {}
+
+void MmsReceiveManager::Init()
+{
+    mmsReceiver_ = std::make_shared<MmsReceive>(slotId_);
+    if (mmsReceiver_ == nullptr) {
+        TELEPHONY_LOGE("failed to create mmsReceiver");
+        return;
+    }
+}
+
+int32_t MmsReceiveManager::DownloadMms(
+    const std::u16string &mmsc, const std::u16string &data, const std::u16string &ua, const std::u16string &uaprof)
+{
+    if (mmsReceiver_ == nullptr) {
+        TELEPHONY_LOGE("mmsReceiver_ is nullptr");
+        return TELEPHONY_ERR_LOCAL_PTR_NULL;
+    }
+
+    int32_t downloadResult = mmsReceiver_->ExecuteDownloadMms(
+        StringUtils::ToUtf8(mmsc), StringUtils::ToUtf8(data), StringUtils::ToUtf8(ua), StringUtils::ToUtf8(uaprof));
+    if (downloadResult == TELEPHONY_ERR_SUCCESS) {
+        TELEPHONY_LOGI("download mms successed");
+        return TELEPHONY_ERR_SUCCESS;
+    } else {
+        TELEPHONY_LOGI("download mms failed");
+        return downloadResult;
+    }
+}
+} // namespace Telephony
+} // namespace OHOS
---
+++ services/mms/mms_sender.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mms_sender.h"
+
+#include "telephony_log_wrapper.h"
+
+namespace OHOS {
+namespace Telephony {
+std::string SEND_METHOD = "POST";
+
+MmsSender::MmsSender(int32_t slotId) : DataRequest(slotId)
+{
+    slotId_ = slotId;
+    CreateRequestId();
+}
+
+MmsSender::~MmsSender() {}
+
+int32_t MmsSender::ExecuteSendMms(
+    const std::string &contentUrl, const std::string &pduDir, const std::string &ua, const std::string &uaprof)
+{
+    auto mmsNetworkMgr = std::make_shared<MmsNetworkManager>();
+    if (mmsNetworkMgr == nullptr) {
+        TELEPHONY_LOGE("mmsNetworkMgr_ is nullptr");
+        return TELEPHONY_ERR_LOCAL_PTR_NULL;
+    }
+
+    std::unique_lock<std::mutex> lock(sendMmsMutex_);
+    return ExecuteMms(SEND_METHOD, mmsNetworkMgr, contentUrl, pduDir);
+}
+} // namespace Telephony
+} // namespace OHOS
