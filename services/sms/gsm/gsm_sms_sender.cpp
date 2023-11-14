c1e2261da3c223a08a1e3030f21aa5c8ca6c3305
liuxiyao223 <liuxiyao223@huawei.com>
2023-11-14 10:17:29 +0800

    修改Cdma数据短信编码问题 Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/sms/gsm/gsm_sms_sender.cpp
+++ services/sms/gsm/gsm_sms_sender.cpp
@@ -50,7 +50,7 @@ void GsmSmsSender::TextBasedSmsDelivery(const string &desAddr, const string &scA
     std::shared_ptr<struct SmsTpdu> tpdu =
         gsmSmsMessage.CreateDefaultSubmitSmsTpdu(desAddr, scAddr, text, isStatusReport, codingType);
     if (tpdu == nullptr) {
-        SendCallbackBecauseTpduNull(sendCallback, "TextBasedSmsDelivery");
+        SendCallbackExceptionCase(sendCallback, "TextBasedSmsDelivery");
         return;
     }
     int cellsInfosSize = static_cast<int>(cellsInfos.size());
@@ -158,154 +158,13 @@ void GsmSmsSender::DataBasedSmsDelivery(const std::string &desAddr, const std::s
     gsmSmsMessage.SplitMessage(cellsInfos, dataStr, CheckForce7BitEncodeType(), codingType, true);
     uint8_t msgRef8bit = GetMsgRef8Bit();
     std::shared_ptr<struct SmsTpdu> tpdu = gsmSmsMessage.CreateDataSubmitSmsTpdu(
-        desAddr, scAddr, port, data, dataLen, msgRef8bit, (deliveryCallback == nullptr) ? false : true);
+        desAddr, scAddr, port, data, dataLen, msgRef8bit, codingType, (deliveryCallback == nullptr) ? false : true);
     if (tpdu == nullptr) {
-        SendCallbackBecauseTpduNull(sendCallback, "DataBasedSmsDelivery");
+        SendCallbackExceptionCase(sendCallback, "DataBasedSmsDelivery");
         return;
     }
-
     DataBasedSmsDeliverySplitPage(
         gsmSmsMessage, cellsInfos, tpdu, msgRef8bit, desAddr, scAddr, port, sendCallback, deliveryCallback);
-}
-
-void GsmSmsSender::DataBasedSmsDeliverySplitPage(GsmSmsMessage &gsmSmsMessage, std::vector<struct SplitInfo> cellsInfos,
-    std::shared_ptr<struct SmsTpdu> tpdu, uint8_t msgRef8bit, const std::string &desAddr, const std::string &scAddr,
-    int32_t port, const sptr<ISendShortMessageCallback> &sendCallback,
-    const sptr<IDeliveryShortMessageCallback> &deliveryCallback)
-{
-    uint32_t cellsInfosSize = static_cast<uint32_t>(cellsInfos.size());
-    for (uint32_t indexData = 0; indexData < cellsInfosSize; indexData++) {
-        const uint8_t *dataItem = reinterpret_cast<uint8_t *>(cellsInfos[indexData].text.data());
-        uint32_t dataItemLen = static_cast<uint32_t>(cellsInfos[indexData].text.size());
-
-        std::shared_ptr<SmsSendIndexer> indexer =
-            make_shared<SmsSendIndexer>(desAddr, scAddr, port, dataItem, dataItemLen, sendCallback, deliveryCallback);
-        if (indexer == nullptr) {
-            SendResultCallBack(indexer, ISendShortMessageCallback::SEND_SMS_FAILURE_UNKNOWN);
-            TELEPHONY_LOGE("DataBasedSmsDeliverySplitPage create SmsSendIndexer nullptr");
-            return;
-        }
-
-        (void)memset_s(tpdu->data.submit.userData.data, MAX_USER_DATA_LEN + 1, 0x00, MAX_USER_DATA_LEN + 1);
-        if (cellsInfos[indexData].encodeData.size() > MAX_USER_DATA_LEN + 1) {
-            TELEPHONY_LOGE("DataBasedSmsDeliverySplitPage data length invalid.");
-            return;
-        }
-        int ret = memcpy_s(tpdu->data.submit.userData.data, MAX_USER_DATA_LEN + 1, &cellsInfos[indexData].encodeData[0],
-            cellsInfos[indexData].encodeData.size());
-        if (ret != EOK) {
-            SendResultCallBack(indexer, ISendShortMessageCallback::SEND_SMS_FAILURE_UNKNOWN);
-            TELEPHONY_LOGE("DataBasedSmsDeliverySplitPage ret:%{public}d", ret);
-            return;
-        }
-        DataBasedSmsDeliveryPacketSplitPage(gsmSmsMessage, tpdu, msgRef8bit, indexData, port, scAddr, sendCallback,
-            deliveryCallback, indexer, cellsInfos);
-    }
-}
-
-void GsmSmsSender::DataBasedSmsDeliveryPacketSplitPage(GsmSmsMessage &gsmSmsMessage,
-    std::shared_ptr<struct SmsTpdu> tpdu, uint8_t msgRef8bit, uint32_t indexData, int32_t port,
-    const std::string &scAddr, const sptr<ISendShortMessageCallback> &sendCallback,
-    const sptr<IDeliveryShortMessageCallback> &deliveryCallback, std::shared_ptr<SmsSendIndexer> indexer,
-    std::vector<struct SplitInfo> cellsInfos)
-{
-    tpdu->data.submit.userData.length = static_cast<int>(cellsInfos[indexData].encodeData.size());
-    tpdu->data.submit.userData.data[cellsInfos[indexData].encodeData.size()] = 0;
-    tpdu->data.submit.msgRef = msgRef8bit;
-
-    int headerCnt = 0;
-    uint32_t cellsInfosSize = static_cast<uint32_t>(cellsInfos.size());
-    if (cellsInfosSize > 1) {
-        indexer->SetIsConcat(true);
-        SmsConcat concat;
-        concat.is8Bits = true;
-        concat.msgRef = msgRef8bit;
-        concat.totalSeg = static_cast<uint16_t>(cellsInfosSize);
-        concat.seqNum = static_cast<uint16_t>(indexData + 1);
-        indexer->SetSmsConcat(concat);
-        headerCnt += gsmSmsMessage.SetHeaderConcat(headerCnt, concat);
-    }
-
-    if (headerCnt >= MAX_UD_HEADER_NUM) {
-        TELEPHONY_LOGE("PDU header Array out of bounds");
-        return;
-    }
-
-    tpdu->data.submit.userData.header[headerCnt].udhType = UDH_APP_PORT_16BIT;
-    tpdu->data.submit.userData.header[headerCnt].udh.appPort16bit.destPort = ((unsigned short)port & 0xFFFF);
-    tpdu->data.submit.userData.header[headerCnt].udh.appPort16bit.originPort = 0;
-    headerCnt++;
-
-    tpdu->data.submit.bHeaderInd = (headerCnt > 0) ? true : false;
-    /* Set User Data Header for Alternate Reply Address */
-    headerCnt += gsmSmsMessage.SetHeaderReply(headerCnt);
-    /* Set User Data Header for National Language Single Shift */
-    DataCodingScheme pCodingType = DATA_CODING_7BIT;
-    MSG_LANGUAGE_ID_T langId = MSG_ID_RESERVED_LANG;
-    headerCnt += gsmSmsMessage.SetHeaderLang(headerCnt, pCodingType, langId);
-    tpdu->data.submit.userData.headerCnt = headerCnt;
-
-    std::shared_ptr<struct EncodeInfo> encodeInfo = gsmSmsMessage.GetSubmitEncodeInfo(scAddr, false);
-    if (encodeInfo == nullptr) {
-        SendResultCallBack(sendCallback, ISendShortMessageCallback::SEND_SMS_FAILURE_UNKNOWN);
-        TELEPHONY_LOGE("DataBasedSmsDeliveryPacketSplitPage encodeInfo nullptr error.");
-        SmsHiSysEvent::WriteSmsSendFaultEvent(slotId_, SmsMmsMessageType::SMS_SHORT_MESSAGE,
-            SmsMmsErrorCode::SMS_ERROR_PDU_ENCODEING_FAIL, "data sms gsm encodeInfo nullptr error");
-        return;
-    }
-
-    if (cellsInfosSize > 1 && indexData < (cellsInfosSize - 1)) {
-        tpdu->data.submit.bStatusReport = false;
-    } else {
-        tpdu->data.submit.bStatusReport = (deliveryCallback == nullptr) ? false : true;
-    }
-    encodeInfo->isMore_ = (cellsInfosSize > 1) ? true : false;
-    DataBasedSmsDeliverySendSplitPage(encodeInfo, sendCallback, indexer, msgRef8bit, cellsInfosSize);
-}
-
-void GsmSmsSender::CharArrayToString(const uint8_t *data, uint32_t dataLen, std::string &dataStr)
-{
-    uint32_t indexData = 0;
-    while (indexData < dataLen) {
-        dataStr += data[indexData];
-        indexData++;
-    }
-}
-
-void GsmSmsSender::SendCallbackBecauseTpduNull(const sptr<ISendShortMessageCallback> &sendCallback, std::string str)
-{
-    SendResultCallBack(sendCallback, ISendShortMessageCallback::SEND_SMS_FAILURE_UNKNOWN);
-    TELEPHONY_LOGE("%{public}s tpdu nullptr error.", str.c_str());
-    SmsHiSysEvent::WriteSmsSendFaultEvent(slotId_, SmsMmsMessageType::SMS_SHORT_MESSAGE,
-        SmsMmsErrorCode::SMS_ERROR_PDU_ENCODEING_FAIL, "data sms gsm encodeInfo nullptr error");
-}
-
-void GsmSmsSender::DataBasedSmsDeliverySendSplitPage(std::shared_ptr<struct EncodeInfo> encodeInfo,
-    const sptr<ISendShortMessageCallback> &sendCallback, shared_ptr<SmsSendIndexer> indexer, uint8_t msgRef8bit,
-    uint32_t cellsInfosSize)
-{
-    std::vector<uint8_t> smca(encodeInfo->smcaData_, encodeInfo->smcaData_ + encodeInfo->smcaLen);
-    std::vector<uint8_t> pdu(encodeInfo->tpduData_, encodeInfo->tpduData_ + encodeInfo->tpduLen);
-    TELEPHONY_LOGE("DataBasedSmsDeliverySendSplitPage cellsInfosSize:%{public}d", cellsInfosSize);
-    std::shared_ptr<uint8_t> unSentCellCount = make_shared<uint8_t>(cellsInfosSize);
-    std::shared_ptr<bool> hasCellFailed = make_shared<bool>(false);
-    if (unSentCellCount == nullptr || hasCellFailed == nullptr) {
-        SendResultCallBack(sendCallback, ISendShortMessageCallback::SEND_SMS_FAILURE_UNKNOWN);
-        return;
-    }
-
-    chrono::system_clock::duration timePoint = chrono::system_clock::now().time_since_epoch();
-    int64_t timeStamp = chrono::duration_cast<chrono::seconds>(timePoint).count();
-    indexer->SetUnSentCellCount(*unSentCellCount);
-    indexer->SetHasCellFailed(hasCellFailed);
-    indexer->SetEncodeSmca(std::move(smca));
-    indexer->SetEncodePdu(std::move(pdu));
-    indexer->SetHasMore(encodeInfo->isMore_);
-    indexer->SetMsgRefId(msgRef8bit);
-    indexer->SetNetWorkType(NET_TYPE_GSM);
-    indexer->SetTimeStamp(timeStamp);
-    std::unique_lock<std::mutex> lock(mutex_);
-    SendSmsToRil(indexer);
 }
 
 void GsmSmsSender::SendSmsToRil(const shared_ptr<SmsSendIndexer> &smsIndexer)
@@ -549,7 +408,7 @@ void GsmSmsSender::ResendDataDelivery(const std::shared_ptr<SmsSendIndexer> &sms
     std::shared_ptr<struct SmsTpdu> tpdu = nullptr;
     tpdu = gsmSmsMessage.CreateDataSubmitSmsTpdu(smsIndexer->GetDestAddr(), smsIndexer->GetSmcaAddr(),
         smsIndexer->GetDestPort(), smsIndexer->GetData().data(), smsIndexer->GetData().size(), msgRef8bit,
-        isStatusReport);
+        smsIndexer->GetDcs(), isStatusReport);
 
     std::shared_ptr<struct EncodeInfo> encodeInfo = nullptr;
     encodeInfo = gsmSmsMessage.GetSubmitEncodeInfo(smsIndexer->GetSmcaAddr(), false);
