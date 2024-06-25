98848c940de80f7ed79f6ef0e0601af49075804a
liyufan <liyufan5@huawei.com>
2024-06-25 17:33:00 +0800

    解决因订阅者数量超限制导致事件发送失败的问题
    Signed-off-by: liyufan <liyufan5@huawei.com>

--- services/sms/sms_broadcast_subscriber_receiver.cpp
+++ services/sms/sms_broadcast_subscriber_receiver.cpp
@@ -14,14 +14,19 @@
  */
 
 #include "sms_broadcast_subscriber_receiver.h"
-
 #include "sms_persist_helper.h"
 #include "telephony_log_wrapper.h"
 #include "want.h"
+#include "event_handler.h"
+#include "event_runner.h"
 
 namespace OHOS {
 namespace Telephony {
 using namespace EventFwk;
+
+std::queue<std::shared_ptr<SmsBroadcastSubscriberReceiver>> sptrQueue;
+std::shared_ptr<AppExecFwk::EventRunner> eventRunner_ = nullptr;
+std::shared_ptr<AppExecFwk::EventHandler> eventHandler_ = nullptr;
 
 SmsBroadcastSubscriberReceiver::SmsBroadcastSubscriberReceiver(const CommonEventSubscribeInfo &subscriberInfo,
     std::shared_ptr<SmsReceiveReliabilityHandler> handler, uint16_t refId, uint16_t dataBaseId,
@@ -48,8 +53,39 @@ void SmsBroadcastSubscriberReceiver::OnReceiveEvent(const OHOS::EventFwk::Common
         if (!address_.empty() && isSms) {
             DelayedSingleton<SmsPersistHelper>::GetInstance()->UpdateContact(address_);
         }
-
         handler_->DeleteMessageFormDb(refId_, dataBaseId_);
+        // only save one object of eventrunner and eventhandler because each object will create a new thread.
+        if (eventRunner_ == nullptr) {
+            eventRunner_ = AppExecFwk::EventRunner::Create("unsubscribe thread");
+        }
+        if (eventHandler_ == nullptr) {
+            eventHandler_ = std::make_shared<AppExecFwk::EventHandler>(eventRunner_);
+        }
+        if (eventHandler_ == nullptr) {
+            TELEPHONY_LOGE("create eventhandler fail");
+            return;
+        }
+        // post a task that be excuted after 10s, and callback is static function in this class.
+        bool postTaskResult = eventHandler_->PostTask(UnsubscribeInCes, "unsubscribe event task", 10000);
+        if (!postTaskResult) {
+            TELEPHONY_LOGE("post unsubscribe task fail");
+        }
+    }
+}
+
+void SmsBroadcastSubscriberReceiver::UnsubscribeInCes()
+{
+    TELEPHONY_LOGE("UnsubscribeInCes entry");
+    if (sptrQueue.empty()) {
+        TELEPHONY_LOGE("SmsBroadcastSubscriberReceiver queue is empty");
+    } else {
+        auto smsBroadcastSubscriberReceiver = sptrQueue.front();
+        sptrQueue.pop();
+        // call api to unsubscribe
+        bool unsubscribeResult = CommonEventManager::UnSubscribeCommonEvent(smsBroadcastSubscriberReceiver);
+        if (!unsubscribeResult) {
+            TELEPHONY_LOGE("unsubscribe fail");
+        }
     }
 }
 } // namespace Telephony
