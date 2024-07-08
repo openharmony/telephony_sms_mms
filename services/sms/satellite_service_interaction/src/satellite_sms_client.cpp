6218d020d2e16aada8dd9eec0fda23da94eb50e7
liuxiyao223 <liuxiyao223@huawei.com>
2024-07-08 16:43:34 +0800

    IssueNo:sms mms仓log重复率整改
    Description:sms mms仓log重复率整改
    Sig:SIG_Telephony
    Feature or Bugfix: Bugfix
    Binary Source: No
    
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/sms/satellite_service_interaction/src/satellite_sms_client.cpp
+++ services/sms/satellite_service_interaction/src/satellite_sms_client.cpp
@@ -60,7 +60,6 @@ sptr<ISatelliteService> SatelliteSmsClient::GetServiceProxy()
     }
     sptr<IRemoteObject> obj = sam->CheckSystemAbility(TELEPHONY_SATELLITE_SERVICE_ABILITY_ID);
     if (obj == nullptr) {
-        TELEPHONY_LOGE("Failed to get satellite service");
         return nullptr;
     }
     std::unique_ptr<SatelliteServiceDeathRecipient> recipient = std::make_unique<SatelliteServiceDeathRecipient>(*this);
@@ -236,7 +235,6 @@ bool SatelliteSmsClient::IsSatelliteEnabled()
 {
     auto proxy = GetServiceProxy();
     if (proxy == nullptr) {
-        TELEPHONY_LOGE("service proxy is null!");
         return false;
     }
     return proxy->IsSatelliteEnabled();
