1498e1c941acd9ed0b73bc106b7e6e7e6b529fc3
lishuo <lishuo92@h-partners.com>
2025-03-31 11:21:42 +0000

    多线程共享指针引用加锁
    Signed-off-by: lishuo <lishuo92@h-partners.com>

--- frameworks/js/napi/src/napi_mms_pdu_helper.cpp
+++ frameworks/js/napi/src/napi_mms_pdu_helper.cpp
@@ -84,6 +84,7 @@ std::string NapiMmsPduHelper::GetDbUrl()
 
 std::shared_ptr<DataShare::DataShareHelper> NapiMmsPduHelper::GetDataShareHelper()
 {
+    std::unique_lock<std::mutex> lock(mtx_);
     return datashareHelper_;
 }
 } // namespace Telephony
