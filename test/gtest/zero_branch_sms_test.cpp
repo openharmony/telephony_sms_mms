e744e3d35b20e133e591762edc5746289a70239a
ohos-fxd <fangxudong4@huawei.com>
2026-01-13 11:39:41 +0800

    fix dt--signoff
    Signed-off-by: ohos-fxd <fangxudong4@huawei.com>

--- test/gtest/zero_branch_sms_test.cpp
+++ test/gtest/zero_branch_sms_test.cpp
@@ -2032,7 +2032,7 @@ HWTEST_F(BranchSmsTest, SmsPersistHelper_0001, Function | MediumTest | Level1)
     smsPersistHelper->QuerySession(predicates, sessionId, messageCount);
     EXPECT_GE(sessionId, 0);
     smsPersistHelper->QuerySmsMmsForId(predicates, dataBaseId);
-    EXPECT_GE(dataBaseId, 0);
+    EXPECT_NE(dataBaseId, 0);
 
     smsPersistHelper->Update(predicates, bucket);
     std::vector<SmsReceiveIndexer> indexers;
@@ -2448,14 +2448,13 @@ HWTEST_F(BranchSmsTest, SmsService_GetSmsShortCodeType, Function | MediumTest | 
 {
     auto smsService = DelayedSingleton<SmsService>::GetInstance();
     int32_t shortCodeType = -1;
-    int32_t slotId = 0;
-    std::string desAddr = "";
+    std::string desAddr = "12345";
     smsService->slotSmsInterfaceManagerMap_.erase(INVALID_SLOTID);
+    EXPECT_EQ(smsService->GetSmsShortCodeType(INVALID_SLOTID, desAddr, shortCodeType), TELEPHONY_ERR_PERMISSION_ERR);
+    AccessMmsToken token;
     EXPECT_EQ(smsService->GetSmsShortCodeType(INVALID_SLOTID, desAddr, shortCodeType), TELEPHONY_ERR_SLOTID_INVALID);
     smsService->slotSmsInterfaceManagerMap_[INVALID_SLOTID] = std::make_shared<SmsInterfaceManager>(INVALID_SLOTID);
-    smsService->slotSmsInterfaceManagerMap_[slotId] = std::make_shared<SmsInterfaceManager>(slotId);
-    EXPECT_GE(smsService->GetSmsShortCodeType(slotId, desAddr, shortCodeType), TELEPHONY_ERR_SUCCESS);
-    EXPECT_EQ(shortCodeType, -1);
+    EXPECT_GE(smsService->GetSmsShortCodeType(INVALID_SLOTID, desAddr, shortCodeType), TELEPHONY_ERR_SUCCESS);
 }
 
 /**
