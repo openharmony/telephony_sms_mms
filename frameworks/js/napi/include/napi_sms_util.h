8cda67c0de615f1d1c9524b874a1b03f0b37f8ee
lizhiqi_hw <lizhiqi14@huawei.com>
2026-01-16 17:43:03 +0800

    new: 新建文件 napi_sms_util.h
    Signed-off-by: lizhiqi_hw <lizhiqi14@huawei.com>

---
+++ frameworks/js/napi/include/napi_sms_util.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NAPI_SMS_UTIL_H
+#define NAPI_SMS_UTIL_H
+
+#include <string>
+#include <vector>
+
+#include "napi_util.h"
+
+namespace OHOS {
+namespace Telephony {
+class NapiSmsUtil {
+public:
+    static __attribute__((noinline)) void Unref(napi_env env, napi_ref ref);
+    static void CloseHandleScopeAndUnrefBoth(
+        napi_handle_scope scope, napi_env env, napi_ref ref1, napi_ref ref2);
+};
+} // namespace Telephony
+} // namespace OHOS
